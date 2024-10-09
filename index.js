bd92d33b20b6709809f6d43d573fb6f3f835241d
HanKyung <infohk.286@gmail.com>
2024-10-09 22:26:32 +0800

    removed http section

--- index.js
+++ index.js
@@ -14,17 +14,6 @@ const HOST = '0.0.0.0';
 const OS = require('os');
 const ENV = 'DEV';
 
-const http = require("http");
-const hostname = "localhost";
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.end("<html><body><h1>Hello, World!</h1></body></html");
-})
-
-server.listen(port, hostname);
 
 // App
 const app = express();
